12abbf74c555729ad488480bbc0d55ce7faaa7b4
RustyNova <rusty.nova.jsb@gmail.com>
2025-12-04 10:20:38 +0100

    refactor: moved core to api_bindium

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,3 +1,33 @@
+use api_bindium::endpoints::EndpointUriBuilder;
+use api_bindium::endpoints::path::EndpointUriBuilderPath;
+
 pub mod user;
 
-pub struct ListenBrainzAPI;
+#[derive(Debug, bon::Builder, Clone)]
+pub struct ListenBrainzAPIEnpoints {
+    /// The domain of the listenbrainz server.
+    ///
+    /// Please note that all the api endpoints must be accessed by HTTPS.
+    #[builder(default = "api.listenbrainz.org".to_string())]
+    lb_domain: String,
+}
+
+impl ListenBrainzAPIEnpoints {
+    /// The api root
+    pub fn api_root(&self) -> String {
+        format!("https://{}", self.lb_domain)
+    }
+
+    /// Return the root Uri for the
+    pub fn endpoint_builder(&self) -> EndpointUriBuilder<EndpointUriBuilderPath> {
+        EndpointUriBuilder::new()
+            .https()
+            .set_authority(&self.lb_domain)
+    }
+}
+
+impl Default for ListenBrainzAPIEnpoints {
+    fn default() -> Self {
+        Self::builder().build()
+    }
+}
--- src/client/http_verb.rs
+++
@@ -1,5 +0,0 @@
-/// The HTTP verb of the api request
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum HTTPVerb {
-    Get,
-}
